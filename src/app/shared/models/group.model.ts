d715d9b57fb6d98d565c69e65ae16955154a1746
Martincttrll <95213604+Martincttrll@users.noreply.github.com>
2023-11-06 12:07:52 +0100

    feat : interfaces models

---
+++ src/app/shared/models/group.model.ts
@@ -0,0 +1,6 @@
+export interface Group {
+  id: number;
+  name: string;
+  token: string;
+  winner: number;
+}
---
+++ src/app/shared/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  nbWin: number;
+  nbGame: number;
+  idSpotify: string;
+  avatar: string;
+}
