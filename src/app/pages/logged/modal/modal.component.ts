4aeec5929c9513266589d39e707903f4854fa2df
Martincttrll <95213604+Martincttrll@users.noreply.github.com>
2023-12-08 10:44:53 +0100

    feat : create group modal

--- src/app/pages/logged/modal/modal.component.ts
+++ src/app/pages/logged/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { GroupService } from 'src/app/shared/services/group.service';
 
 @Component({
   selector: 'app-modal',
@@ -8,7 +9,25 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class ModalComponent {
   @Output() closeModalEvent = new EventEmitter<void>();
 
+  showCreateGroup: boolean = false;
+  constructor(public group: GroupService) {}
+
   sendCloseModal() {
     this.closeModalEvent.emit();
+  }
+  changeModal() {
+    this.showCreateGroup = true;
+  }
+  createGroup(inputElement: any) {
+    // document.getElementById('group-name').value;
+    const groupname = inputElement.value;
+    this.group.createGroup(groupname).then((data) => {
+      console.log(data);
+    });
+  }
+  copyInputMessage(inputElement: any) {
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
   }
 }
