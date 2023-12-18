2c84de3400b3ff767082ed25c0f23f2735618350
Martincttrll <95213604+Martincttrll@users.noreply.github.com>
2023-12-18 18:29:04 +0100

    fix : player audio auto

--- src/app/pages/logged/game/game.component.ts
+++ src/app/pages/logged/game/game.component.ts
@@ -11,6 +11,7 @@ import { WebsocketService } from 'src/app/shared/services/websocket.service';
   styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
+  @ViewChild('audioPlayer') audioPlayer!: ElementRef;
   constructor(
     private ws: WebsocketService,
     private http: HttpService,
@@ -47,26 +48,46 @@ export class GameComponent {
 
   submitGuess(input: any) {
     const guess = input.value;
+    console.log(guess);
+    input.value = '';
     this.http
       .requestApi('/api/game/try', 'POST', {
         input: guess,
         player_id: this.http.user!.id,
         title_points: this.guessTitle,
+        group_token: this.groupToken,
         artist_points: this.guessArtist,
         song_id: this.currentSong.id,
       })
       .then((data) => {
         console.log(data);
+        let message = data.message;
+        switch (data.guess) {
+          case 'artist':
+            console.log('Option 1 sélectionnée');
+            this.startManche();
+            break;
+
+          case 'title':
+            console.log('Option 2 sélectionnée');
+            this.startManche();
+            break;
+
+          case null:
+            console.log('Option 3 sélectionnée');
+            break;
+
+          default:
+            console.log('Pas trouvé');
+        }
       });
   }
-  ////NE marche pas, il faut déclanche l'audio automatiquement
+
   playAudio() {
     if (this.isUrlReady) {
-      const audioPlayer = document.getElementById(
-        'audioPlayer'
-      ) as HTMLAudioElement;
-      console.log(audioPlayer);
+      const audioPlayer = this.audioPlayer.nativeElement as HTMLAudioElement;
       if (audioPlayer) {
+        audioPlayer.load();
         audioPlayer.play();
       }
     }
