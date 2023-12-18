f2975fb572fa4bfc7503e1066eaf7e7e15b3b939
Martincttrll <95213604+Martincttrll@users.noreply.github.com>
2023-12-18 17:06:06 +0100

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,5 +4,6 @@ export const MIX_PUSHER_APP_KEY = '80c8c2be396fa498bc42';
 export const MIX_PUSHER_HOST = 'localhost';
 export const MIX_PUSHER_PORT = 6001;
 export const MIX_PUSHER_PORT_TLS = 6001;
+export const MIX_PUSHER_CLUSTER = 'eu';
 export const MIX_PUSHER_FORCE_TLS = false;
 export const MIX_PUSHER_TRANSPORT = ['ws'];
