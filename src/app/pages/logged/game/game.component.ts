2dc8afe8c9b65855498130d11b689e3f55d351a8
Martincttrll <95213604+Martincttrll@users.noreply.github.com>
2024-01-09 20:33:22 +0100

    feat : chat & reponse

--- src/app/pages/logged/game/game.component.ts
+++ src/app/pages/logged/game/game.component.ts
@@ -16,10 +16,19 @@ export class GameComponent {
   @ViewChild("audioPlayer") audioPlayer!: ElementRef;
   guessTitle: number = 200;
   guessArtist: number = 200;
+  chatMessages: {
+    user: string;
+    message: string;
+    status: string | undefined;
+  }[] = [];
   groupToken: string = "";
   currentSong?: any;
+  showCurrentSong: boolean = false;
   isUrlReady: boolean = false;
   group?: Group;
+
+  nbManche: number = 5;
+  currentManche: number = 0;
   constructor(
     private ws: WebsocketService,
     private http: HttpService,
@@ -41,9 +50,17 @@ export class GameComponent {
     });
   }
 
+  ngOnDestroy(): void {
+    this.groupService.leaveGroup(this.groupToken);
+  }
+
   loadGroupData(): void {
     this.groupService.getGroupByToken(this.groupToken).then((data) => {
       this.group = data;
+      this.group.users.forEach((user) => {
+        user.points = 0;
+        user.alreadyGuess = [];
+      });
       // this.ws.linkChannelWithToken(this.group.token, this.group);
     });
   }
@@ -56,55 +73,129 @@ export class GameComponent {
   }
 
   submitGuess(input: any) {
+    const player = this.group?.users.find(
+      (user) => user.id === this.http.user!.id
+    );
     const guess = input.value;
-    console.log(guess);
     input.value = "";
     this.http.requestApi("/api/game/try", "POST", {
       input: guess,
       player_id: this.http.user!.id,
-      title_points: this.guessTitle,
       group_token: this.groupToken,
+      title_points: this.guessTitle,
       artist_points: this.guessArtist,
       song_id: this.currentSong.id,
+      alreadyGuess: player!.alreadyGuess,
     });
   }
 
   handleGuessResponse(guessAnswer: any) {
-    const guessType = guessAnswer.datas.original.guess;
-    switch (guessType) {
-      case "artist":
-        console.log(guessAnswer);
-        this.getRandomTrack();
-        break;
+    const data = guessAnswer.datas.original;
 
-      case "title":
-        console.log(guessAnswer);
-        this.getRandomTrack();
-        break;
+    const userPoints = data.user_points ?? 0;
 
-      case null:
-        console.log(guessAnswer);
-        break;
+    const player = this.group?.users.find((user) => user.id === data.player);
 
-      default:
-        console.log("Pas trouvé");
+    let guessStatus = undefined;
+
+    if (data.guess === "artist" || data.guess === "title") {
+      if (player) {
+        player.points += userPoints;
+      }
+
+      if (data.guess === "artist") {
+        this.guessArtist = data.artist_points;
+        player?.alreadyGuess?.push("artist");
+      } else if (data.guess === "title") {
+        this.guessTitle = data.title_points;
+        player?.alreadyGuess?.push("title");
+      }
+
+      guessStatus = "found";
+    } else {
+      console.log("Pas trouvé");
+    }
+
+    const playerName = player?.name || "Inconnu";
+    const message = data.message;
+
+    this.chatMessages.push({
+      user: playerName,
+      message: message,
+      status: guessStatus,
+    });
+
+    if (this.chatMessages.length > 8) {
+      this.chatMessages.shift();
     }
   }
 
   handleNewRandomSong(song: any) {
-    this.currentSong = song[0].original.song;
-    console.log(this.currentSong.id);
-    this.isUrlReady = true;
-    this.playAudio();
+    ///Pour attendre que les infos du groupes remontent bien (a changer pour un observer/subscriber)
+    setTimeout(() => {
+      this.guessArtist = 200;
+      this.guessTitle = 200;
+      this.group!.users.forEach((user) => {
+        user.alreadyGuess = [];
+      });
+      this.currentSong = song[0].original.song;
+      console.log(this.currentSong.id);
+      this.isUrlReady = true;
+      this.playAudio();
+    }, 1000);
   }
+
   playAudio() {
-    if (this.isUrlReady) {
-      const audioPlayer = this.audioPlayer.nativeElement as HTMLAudioElement;
-      if (audioPlayer) {
-        audioPlayer.volume = 0.3;
-        audioPlayer.load();
-        audioPlayer.play();
+    if (this.currentManche < this.nbManche) {
+      if (this.isUrlReady) {
+        const audioPlayer = this.audioPlayer.nativeElement as HTMLAudioElement;
+        if (audioPlayer) {
+          audioPlayer.volume = 0.3;
+          audioPlayer.load();
+          audioPlayer.play();
+          audioPlayer.addEventListener("ended", () => {
+            this.http
+              .requestApi("/api/song/" + this.currentSong.id, "GET")
+              .then((data) => {
+                this.currentSong.title = data.title;
+                this.currentSong.artist = data.artist;
+                console.log(data);
+                this.showCurrentSong = true;
+              });
+            setTimeout(() => {
+              this.showCurrentSong = false;
+              this.currentSong.title = undefined;
+              this.currentSong.artist = undefined;
+              this.getRandomTrack();
+            }, 4000);
+          });
+        }
       }
+      this.currentManche++;
+    } else {
+      console.log("Jeu fini, le nombre maximal de manche a été atteint");
+    }
+  }
+
+  /////Affichage composant
+  sortedUsers(): User[] {
+    return (
+      this.group?.users
+        .slice()
+        .sort((a, b) => (b.points || 0) - (a.points || 0)) || []
+    );
+  }
+
+  getPodiumClass(index: number): string {
+    switch (index) {
+      case 0:
+        return "first-place";
+      case 1:
+        return "second-place";
+      case 2:
+        return "third-place";
+      default:
+        return "";
     }
   }
 }
