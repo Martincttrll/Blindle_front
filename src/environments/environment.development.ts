6c547d17f84b03e381fcb808821380306df5016e
Martincttrll <95213604+Martincttrll@users.noreply.github.com>
2025-01-31 09:32:08 +0100

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const API_URL: string = 'http://127.0.0.1:8000';
 
-export const MIX_PUSHER_APP_KEY = '80c8c2be396fa498bc42';
+export const MIX_PUSHER_APP_KEY = '';
 export const MIX_PUSHER_HOST = 'localhost';
 export const MIX_PUSHER_PORT = 6001;
 export const MIX_PUSHER_PORT_TLS = 6001;
